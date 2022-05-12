8c458c66d8a3788d31f33681bdbeba603da01953
Carter Ward <20886719+CarterJWard@users.noreply.github.com>
2022-05-12 16:52:24 +1000

    Buttons and function structure added basic operation and operator function changed button sizes for ease of use added buttons for basic operators

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const add = () => {
-
+const add = (n1, n2) => {
+    return n1 + n2;
 }
 
-const subtract = () => {
-
+const subtract = (n1, n2) => {
+    return n1 - n2;
 }
 
-const multiply = () => {
-
+const multiply = (n1, n2) => {
+    return n1 * n2;
 }
 
-const divide = () => {
+const divide = (n1, n2) => {
+    if (n1 == 0 || n2 == 0) {
+        return 0;
+    }
+    return n1 / n2;
+}
 
+const operate = (n1, n2, op) => {
+    switch (op) {
+        case add: add(n1, n2);
+        case sub: subtract(n1, n2);
+        case multi: multiply(n1, n2);
+        case divide: divide(n1, n2);
+        default: return 0;
+    }
 }
