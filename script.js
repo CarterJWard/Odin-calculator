bf9167610124c37a1da75d41ddada5623a83bafb
Carter Ward <20886719+CarterJWard@users.noreply.github.com>
2022-05-30 21:14:18 +1000

    End of day work Calculator works once but doesn't work outside of that case

--- script.js
+++ script.js
@@ -1,28 +1,63 @@
-const add = (n1, n2) => {
-    return n1 + n2;
-}
+let first = 0;
+let second = 0;
+let sec = false;
+let op = "";
 
-const subtract = (n1, n2) => {
-    return n1 - n2;
-}
-
-const multiply = (n1, n2) => {
-    return n1 * n2;
-}
-
-const divide = (n1, n2) => {
-    if (n1 == 0 || n2 == 0) {
-        return 0;
-    }
-    return n1 / n2;
-}
-
-const operate = (n1, n2, op) => {
+const operate = () => {
     switch (op) {
-        case add: add(n1, n2);
-        case sub: subtract(n1, n2);
-        case multi: multiply(n1, n2);
-        case divide: divide(n1, n2);
-        default: return 0;
+        case "add":
+            first = first + second;
+            break;
+        case "sub":
+            first = first - second;
+            break;
+        case "multi":
+            first = first * second;;
+            break;
+        case "divide":
+            if (first == 0 || second == 0) {
+                return 0;
+            } else {
+                first = first / second;
+            }
+            break;
+    }
+    sec = false;
+    update()
+}
+
+const update = () => {
+
+    let display = document.querySelector('#display')
+    if (sec) {
+        display.innerHTML = second
+        console.log(second)
+    } else {
+        display.innerHTML = first
+        console.log(first)
+    }
+}
+
+window.onload = () => {
+    for (i = 0; i <= 9; i++) {
+        let temp = i; //Instance variable is required so query selector access correct Id's
+        document.getElementById(temp.toString()).onclick = () => {
+            if (sec) {
+                second = temp;
+            } else {
+                first = temp;
+            }
+            update();
+        }
+    }
+    let buttons = ["add", "sub", 'multi', 'div']
+    buttons.forEach(element => {
+        document.querySelector('#' + element).onclick = () => {
+            op = element;
+            sec = true
+        }
+    });
+    document.querySelector('#eq').onclick = () => {
+        operate();
     }
 }
